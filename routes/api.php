7df35dff939679cffa7664e87c28151a893e3696
ThawZin999 <egthawzin@gmail.com>
2024-02-01 15:37:16 +0630

    added api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/app-name', fn()=>response()->json([
+    'app_name' => "Laravel on Vercel"
+]));
